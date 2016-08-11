6097f7dd37a651f60464b23720d8180e5d28f05c
unknown <>
2016-08-12 03:00:23 +0900

    bid_count 구현 중

--- account/views.py
+++ account/views.py
@@ -24,12 +24,11 @@ def sign_up(request):
                 authenticated_user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password1'])
                 login(request,authenticated_user)
                 messages.success(request,'환영합니다') #로그인 완료
-                return redirect(settings.LOGIN_URL)
+                return redirect('/')
         else:
             form = ProfileForm()
             return render(request,"account/sign_up.html",{
                 'form':form,
-                'user':user,
                })
     else:
         messages.info(request,'이미 로그인되어있습니다. 로그아웃 이후 실행해주세요.')
@@ -43,7 +42,6 @@ def profile(request):
     return render(request, 'account/profile.html', {
         'user':user,
         'profile' : profile,
-
     })
 
 @login_required
@@ -74,12 +72,12 @@ def mentor_info_edit(request):
     user = get_object_or_404(User, pk=request.user.pk)
 
     if request.method == 'POST':
-        form = MentorInfoForm(request.POST, instance = user.profile)
+        form = MentorInfoUpdateForm(request.POST, instance = user.profile)
         if form.is_valid():
             form.save()
             return redirect('mentor_info_edit')
     else:
-        form = MentorInfoForm(request.POST, instance = user.profile)
+        form = MentorUpdateForm(request.POST, instance = user.profile)
     return render(request, 'account/mentor_info_edit.html',{'user':user, 'form':form,})
 
 @login_required
