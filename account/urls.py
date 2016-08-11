92223bf061de26174bb000464b4578c8b5b5e796
HuiwonKo <dreams96@naver.com>
2016-08-11 17:51:44 +0900

    logout

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', login, name='login'),
+    url(r'^logout/$',logout, name='logout'),
     url(r'^sign_up/$', views.sign_up, name='sign_up'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile_edit/$', views.profile_edit, name='profile_edit'),
